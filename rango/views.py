6833169d06fff8aefe1408131a1337ee29b23e0a
Rahul Walkar <rahulwalkar89@gmail.com>
2017-06-14 15:35:30 +0530

    Updated settings for static and media files

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,10 @@
-from django.http import  HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    name = {'name': "Rahul Walkar"}
+    return render(request, 'rango/about.html', context=name)
